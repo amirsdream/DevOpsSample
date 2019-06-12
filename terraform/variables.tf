cdad99b0fffe5bcb41ca0be54d809fe195b47b62
amirsdream <amirsdream@gmail.com>
2019-06-13 01:49:15 +0300

    betik devops sample

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+# AWS Config
+
+variable "aws_access_key" {
+  default = "XXXXXXXXXXXXXXXXXXX"
+}
+
+variable "aws_secret_key" {
+  default = "XXXXXXXXXXXXXXXXXXX"
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
